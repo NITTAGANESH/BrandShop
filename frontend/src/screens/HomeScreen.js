7bb4c46fe1fd8dff4061a8acd8169b6a1e8c7b85
Your Name <nittaganeshkumar@gmail.com>
2023-01-22 22:42:28 +0530

    adding backend-end server

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-template-curly-in-string */
+import { Link } from "react-router-dom";
+import data from "../data";
+
+function HomeScreen() {
+    return (
+        <div>
+            <h1>Freatured products</h1>
+            <div className="Products">
+                {
+                    data.products.map(product => (
+                        <div className="product" key={product.slug}>
+                            <Link to={`/product/${product.slug}`}>
+                                <img src={product.image} alt={product.name} />
+                            </Link>
+                            <div className='product-info'>
+                                <Link to={`/product/${product.slug}`}>
+                                    <p>
+                                        {product.name}
+                                    </p>
+                                </Link>
+                                <p>
+                                    <strong>${product.price}</strong>
+                                </p>
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
+                    )
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default HomeScreen;
